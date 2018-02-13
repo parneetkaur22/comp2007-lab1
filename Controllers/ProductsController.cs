f1d2ae4534faa869603068a638e308987d205002
parneetkaur22 <31783681+parneetkaur22@users.noreply.github.com>
2018-02-13 17:33:20 -0500

    first commit

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace lab1.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: Products
+        public ActionResult Index()
+        {
+            return View();
+        }
+          // Get: Products/details?acer=[value]
+          public ActionResult Details(string acer)
+        {
+            // check for an empty acer
+            if (acer == null)
+            {
+                //return View("Error");
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.Acer = acer;
+            return View();
+        }
+    }
+}
